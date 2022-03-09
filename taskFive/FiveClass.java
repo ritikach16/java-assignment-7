a0330e5d2a2f87e850451e904f6eb1406ff237ba
Ritika Chauhan <ritika.chauhan@zemosolabs.com>
2022-03-09 20:45:21 +0530

    5th java assignment

--- taskFive/FiveClass.java
+++ taskFive/FiveClass.java
@@ -3,7 +3,16 @@ package taskFive;
 public class FiveClass {
     public class InnerClass{
         InnerClass(int n){
-            System.out.println("number: ");
+            System.out.println("number: "+n);
+        }
+    }
+}
+
+class SecondClass{
+    public class SecondInnerClass extends FiveClass.InnerClass {
+
+        SecondInnerClass(int n) {
+            super(n);
         }
     }
 }
